d0b4c97358aacf5a46ff003bdad000609ae0cb08
zhangwenqi <zhangwenqi@ruijie.com.cn>
2020-11-05 15:18:16 +0800

    v1.0更新

--- app/router.py
+++ app/router.py
@@ -5,12 +5,20 @@ import os
 from flask import Flask
 from flask_session import Session
 from app.pkg.database.redis_db.client import RedisTool
+from app.blog.router import blog_app
 
 class Service(object):
     def __init__(self):
         self._app = Flask('test_api', instance_relative_config=True)
         self.set_session()
         self.add_app()
+
+    # 添加路由分组
+    def add_app(self):
+        # 添加特征提取分组
+        self._app.register_blueprint(
+            blog_app, url_prefix='/blog'
+        )
 
     # 添加session设置
     def set_session(self):
